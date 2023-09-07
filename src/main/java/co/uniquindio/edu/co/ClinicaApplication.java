2f7154eba0f8b7495e5114d4fbf36525de35df98
Steven <jhoans.murillog@uqvirtual.edu.co>
2023-09-07 10:27:01 -0500

    Primera versión del proyecto

---
+++ src/main/java/co/uniquindio/edu/co/ClinicaApplication.java
@@ -0,0 +1,13 @@
+package co.uniquindio.edu.co;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClinicaApplication.class, args);
+    }
+
+}
